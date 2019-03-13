516cccdfffb1c1296de281ac08e22e083a6ce1ae
lktuser1 <48331029+lktuser1@users.noreply.github.com>
2019-03-12 21:25:17 -0500

    Fresh Commit
    Loaded Visual Studio Project.

---
+++ testgitinvisualstudio/winform/Array.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace winform
+{
+    public partial class Array : Form
+    {
+        string[] str = new string[10];
+        int[] i;
+        ArrayList objArray = new ArrayList();
+        public Array()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i < str.Length;i++)
+            {
+                if (str[i] == null)
+                {
+                    str[i] = textBox1.Text;
+                    break;
+
+                }
+            }
+            listBox1.DataSource = null;
+            listBox1.Items.Clear();
+            listBox1.DataSource = str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            objArray.Add(textBox1.Text);
+            listBox1.DataSource = null;
+            listBox1.Items.Clear();
+            listBox1.DataSource = objArray;
+        }
+
+    }
+}
---
+++ testgitinvisualstudio/winform/ArrayandArrayListPerformance.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+
+namespace winform
+{
+    public partial class ArrayandArrayListPerformance : Form
+    {
+        int[] intarray = new int[10];
+        ArrayList objArrayList = new ArrayList();
+        int i;
+
+        Stopwatch stopWatch = new Stopwatch();
+        public ArrayandArrayListPerformance()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stopWatch.Start();
+            for (i = 0; i < 10; i++)
+                intarray[i] = DateTime.Now.Second;
+
+
+            foreach(int i in intarray)
+             {
+                
+                    
+            }
+            stopWatch.Stop();
+            label1.Text = stopWatch.ElapsedTicks.ToString();
+
+            stopWatch.Start();
+            for (i = 0; i < 10; i++)
+                objArrayList.Add(DateTime.Now.Second);
+
+
+            foreach (int i in objArrayList)
+            {
+
+
+            }
+            stopWatch.Stop();
+            label2.Text = stopWatch.ElapsedTicks.ToString();
+
+        }
+    }
+}
